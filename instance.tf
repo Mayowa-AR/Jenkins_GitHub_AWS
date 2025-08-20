9c3f171ea8406682e6ab52d465ed32151e9bcc37
Mayowa-Ar <amos.arowolo@gmail.com>
2025-08-20 20:19:17 +0100

    updated user_data

--- instance.tf
+++ instance.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "jk_instance" {
   security_groups = [aws_security_group.jenkins_sg.name]
   ami = var.instance_ami
   key_name = var.key_name
+  user_data = file("user_data.sh")
+  count = 2
 
   tags = {
     Name = "JKTerraformInstance"
